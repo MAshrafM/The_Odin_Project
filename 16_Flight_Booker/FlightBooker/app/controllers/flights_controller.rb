a29cdd31ee45b6ea3e84549a4eb116aaf1f5a619
MAshraf <ee.mashraf@gmail.com>
2016-04-16 13:09:52 +0200

    screen 2

--- 16_Flight_Booker/FlightBooker/app/controllers/flights_controller.rb
+++ 16_Flight_Booker/FlightBooker/app/controllers/flights_controller.rb
@@ -4,10 +4,11 @@ class FlightsController < ApplicationController
 	@dates = Flight.list
 	@departure_airport = params[:departure_airport_id]
 	@destination_airport = params[:destination_airport_id]
-	@depart_date = params[:departure_date]
+	@depart_date = Date.parse(params[:departure_date]) if params[:departure_date]
 	@passenger_options = [1,2,3,4]
 	@passengers = params[:passengers]
 	@flights = Flight.search(@departure_airport, @destination_airport, @depart_date)
+	#@flights = Flight.all
 	
 	if params[:commit] == "Search"
 		if @departure_airport == @destination_airport
