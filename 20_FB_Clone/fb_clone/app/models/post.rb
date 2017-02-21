14e69fb174a76e352e6bd2d915d2e56d5350d83f
MAshraf <ee.mashraf@gmail.com>
2017-02-21 22:00:28 +0200

    posts model

---
+++ 20_FB_Clone/fb_clone/app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  
+  belongs_to :user
+  
+  validate :body_or_image
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates :body, length: { maximum: "1000" }
+  
+  def body_or_image
+    errors.add(:at_least_one, "You must provide a body or an image.") if body.blank? && post_image.blank?
+  end
+end
--- 20_FB_Clone/fb_clone/db/schema.rb
+++ 20_FB_Clone/fb_clone/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170211175310) do
+ActiveRecord::Schema.define(version: 20170221195426) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "post_image_file_name"
+    t.string   "post_image_content_type"
+    t.integer  "post_image_file_size"
+    t.datetime "post_image_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                      default: "", null: false
