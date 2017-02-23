fd5b7678ffb3e9d706bd51ce19b41dcfd1eb0adf
MAshraf <ee.mashraf@gmail.com>
2017-02-23 07:18:47 +0200

    comment model

---
+++ 20_FB_Clone/fb_clone/app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :post
+  
+  validates :body,  presence: true, length: { maximum: "1000" }
+end
--- 20_FB_Clone/fb_clone/db/schema.rb
+++ 20_FB_Clone/fb_clone/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221195426) do
+ActiveRecord::Schema.define(version: 20170223051319) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
