0e8f10b088ad5a26a7b1a309e926eef28b18726b
MAshraf <ee.mashraf@gmail.com>
2017-02-25 22:22:01 +0200

    add guard and rspec

--- 20_FB_Clone/fb_clone/app/models/post.rb
+++ 20_FB_Clone/fb_clone/app/models/post.rb
@@ -1,15 +1,11 @@
 class Post < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :post_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   
   belongs_to :user
   has_many :comments
   has_many :likes, dependent: :destroy
   
-  validate :body_or_image
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_content_type :post_image, content_type: /\Aimage\/.*\Z/
   validates :body, length: { maximum: "1000" }
   
-  def body_or_image
-    errors.add(:at_least_one, "You must provide a body or an image.") if body.blank? && post_image.blank?
-  end
 end
--- 20_FB_Clone/fb_clone/spec/models/post_spec.rb
+++ 20_FB_Clone/fb_clone/spec/models/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to, or delete, a #{__FILE__}"
+  
+  it {should validate_presence_of :body}
+  it {should validate_length_of(:body).is_at_most(1000)}
 end
