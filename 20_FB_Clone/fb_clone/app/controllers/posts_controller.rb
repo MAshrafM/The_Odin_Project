5cdea9378e15ccd90a24675f4823fb94dc3cdd23
MAshraf <ee.mashraf@gmail.com>
2017-03-02 16:50:41 +0200

    add some js

--- 20_FB_Clone/fb_clone/app/controllers/posts_controller.rb
+++ 20_FB_Clone/fb_clone/app/controllers/posts_controller.rb
@@ -11,8 +11,17 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
+      respond_to do |format|
+        format.html{ redirect_to root_url }
+        format.json { render 'static_pages/home', status: :created }
+        format.js {}
+      end
       flash[:success] = "Your post has been posted successfully"
     else
+      respond_to do |format|
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+        format.js {}
+      end
       flash[:error] = "Something went wrong"
     end
     redirect_to root_url
@@ -21,7 +30,11 @@ class PostsController < ApplicationController
   def destroy
     @post= Post.find(parmas[:id])
     if @post.destroy
-      redirect_to root_url
+      respond_to |format| do
+        format.html {redirect_to root_url}
+        format.js {}
+      end
+      
     end
   end
   private
