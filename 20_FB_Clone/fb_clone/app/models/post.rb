46858e034bd8079611e3c187229af160ccbc21ff
MAshraf <ee.mashraf@gmail.com>
2017-03-06 14:53:35 +0200

    likes post method

--- 20_FB_Clone/fb_clone/app/models/post.rb
+++ 20_FB_Clone/fb_clone/app/models/post.rb
@@ -11,4 +11,25 @@ class Post < ActiveRecord::Base
   validates_presence_of :user_id
   validates :body, length: { maximum: 1000 }
   
+  #Post Method
+  def liked_by(user)
+    self.likes.create(user_id: user.id) if !liked_by?(user)
+  end
+  
+  def liked_by?(user)
+    !get_like(user).nil?
+  end
+  
+  def get_liked_by(user)
+    get_like(user)
+  end
+  
+  def not_liked_by(user)
+    get_like(user).destroy
+  end
+  
+  private
+  def get_like(user)
+    Like.find_by(user_id: user.id, post_id: self.id)
+  end
 end
--- 20_FB_Clone/fb_clone/db/schema.rb
+++ 20_FB_Clone/fb_clone/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303124231) do
+ActiveRecord::Schema.define(version: 20170306122056) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "body"
@@ -34,8 +34,12 @@ ActiveRecord::Schema.define(version: 20170303124231) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.integer  "post_id"
   end
+
+  add_index "likes", ["post_id"], name: "index_likes_on_post_id"
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id"
 
   create_table "notifications", force: :cascade do |t|
     t.datetime "created_at", null: false
