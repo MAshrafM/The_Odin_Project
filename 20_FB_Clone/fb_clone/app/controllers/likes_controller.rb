915fb6183759da99d5ab5446c89984a7985273a0
MAshraf <ee.mashraf@gmail.com>
2017-03-08 16:34:52 +0200

    likes controller

---
+++ 20_FB_Clone/fb_clone/app/controllers/likes_controller.rb
@@ -0,0 +1,23 @@
+class LikesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    respond do |fotmat|
+      if @post.liked_by(current_user)
+        format.js
+        format.html {redirect_to root_url}
+      end
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @like = Like.find(params[:id])
+    respond do |fotmat|
+      if @like.destroy
+        format.js
+        format.html {redirect_to root_url}
+      end
+    end
+  end
+end
---
+++ 20_FB_Clone/fb_clone/test/controllers/likes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LikesControllerTest < ActionController::TestCase
+  def test_sanity
+    flunk "Need real tests"
+  end
+end
