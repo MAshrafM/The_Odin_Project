7ebdaf0d39ee90f54600d4c09aa19036142fd2f7
MAshraf <ee.mashraf@gmail.com>
2017-03-09 17:47:51 +0200

    comments methods

--- 20_FB_Clone/fb_clone/app/controllers/comments_controller.rb
+++ 20_FB_Clone/fb_clone/app/controllers/comments_controller.rb
@@ -1,11 +1,26 @@
 class CommentsController < ApplicationController
 
   def create
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.build(body: params[:comment][:body], post_id: @post.id)
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to root_url }
+        format.js
+      end
+    end
   end
   
   def update
   end
   
   def destroy
+    @comment = current_user.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to root_url }
+        format.js
+      end
+    end
   end
 end
